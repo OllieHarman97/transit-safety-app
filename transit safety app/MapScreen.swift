db915ad3a474f49ca9848820d2f52dc8741fc87e
OllieHarman97 <oh2212@columbia.edu>
2020-07-27 15:19:37 +0100

    Reverted to Shannon's master

--- transit safety app/MapScreen.swift
+++ transit safety app/MapScreen.swift
@@ -5,36 +5,45 @@
 //  Created by Oliver Harman on 19/07/2020.
 //  Copyright © 2020 Shannon Jin. All rights reserved.
 //
-
 import SwiftUI
 import MapKit
-import GoogleMaps
+
 
 
 struct MapScreen: View {
-    @ObservedObject var viewRouter: ViewRouter
-    @State var input: String = ""
-
     
+    @ObservedObject var viewRouter: ViewRouter
+    @State private var start = "Times Sq-42 St"
     var body: some View {
+                
         ZStack{
-            Image("MenuBar").resizable()
-                .frame(width: 377.0, height: 200)
-                .offset(y:-244)
             
-            TextField("From", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            Image("MenuBar").resizable().onTapGesture {
+                self.viewRouter.currentPage = "login"
+            }.frame(width: 377.0, height: 200)
+            .offset(y:-244)
+            
+            TextField("From", text:  $start)
                 .offset(x:76,y:-226)
             
-            TextField("To", text: $input,onCommit: {
-            }).offset(x:76, y:-268)
+            TextField("To", text:  /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/).offset(x:76, y:-268)
             
+            MapView().frame(width: 377.0, height: 400, alignment: .center)
             
+            goButton().onTapGesture {
+                self.viewRouter.currentPage = "map result"
+            }
             
+            graphButton().onTapGesture {
+                self.viewRouter.currentPage = "monday"
+            }
+
             
-            GoogleMapView()
-                .frame(width: 377.0, height: 400, alignment: .center)
-            
-            
+            tabButton().onTapGesture {
+               if let url = URL(string: "https://public.tableau.com/profile/kathy.lin1766#!/vizhome/stationdensitytry2/Dashboard1?publish=yes") {
+                   UIApplication.shared.open(url)
+               }
+            }
         }
     }
 }
@@ -45,19 +54,69 @@ struct MapScreen_Previews: PreviewProvider {
     }
 }
 
-struct GoogleMapView: UIViewRepresentable {
+struct MapView: UIViewRepresentable {
     
-    private let zoom: Float = 15.0
-    // 1.
-    func makeUIView(context: Self.Context) -> GMSMapView {
-        let camera = GMSCameraPosition.camera(withLatitude: 40.7128, longitude: 74.0060, zoom: 12.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        return mapView
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+         MKMapView(frame: .zero)
     }
     
     // 2.
-    func updateUIView(_ mapView: GMSMapView, context: Context) {
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
         // 3.
+        let location = CLLocationCoordinate2D(latitude: 40.7580, longitude: -73.9855)
+        //(latitude: 40.759011, longitude: -73.984472) A little to the side of grand centrsl
         
-        }
+        // 4.
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        uiView.setRegion(region, animated: true)
+        
+        // 5.
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Times Square"
+        //annotation.subtitle = "London"
+        uiView.addAnnotation(annotation)
+        
     }
+}
+
+struct goButton : View{
+      var body: some View {
+          return Text("Find Safest Path")
+              .foregroundColor(.white)
+              .frame(width: 300, height: 30)
+              .background(Color.blue)
+              .cornerRadius(15)
+              .padding(.top, 50)
+              .font(.system(size:13))
+              .position(x:185, y:530)
+      }
+  }
+
+
+struct tabButton : View{
+    var body: some View {
+        return Text("See hourly subway station density map")
+            .foregroundColor(.white)
+            .frame(width: 300, height: 30)
+            .background(Color.blue)
+            .cornerRadius(15)
+            .padding(.top, 50)
+            .font(.system(size:13))
+            .position(x:185, y:580)
+    }
+}
+
+struct graphButton : View{
+    var body: some View {
+        return Text("See historical station density")
+            .foregroundColor(.blue)
+            .frame(width: 250, height: 30)
+            .background(Color.white)
+            .cornerRadius(15)
+            .padding(.top, 50)
+            .font(.system(size:13))
+            .position(x:240, y:130)
+    }
+}
